633688824d1f5d82c670a37cf3c7d77074d54b22
Rio-OMR21 <orinaryaga21@gmail.com>
2023-05-02 22:25:30 +0300

    my web
    a cv website

---
+++ myjs.js
@@ -0,0 +1,96 @@
+document.getElementById("btn1").addEventListener("click",showtable1);
+function showtable1() {
+    document.getElementById("edu").style.display = "block";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+    
+}
+
+document.getElementById("btn2").addEventListener("click",showtable2);
+function showtable2() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "block";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+    
+    
+}
+
+document.getElementById("btn3").addEventListener("click",showtable3);
+function showtable3() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+    
+    
+}
+document.getElementById("btn4").addEventListener("click",showtable4);
+function showtable4() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "block";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+    
+    
+}
+document.getElementById("btn5").addEventListener("click",showtable5);
+function showtable5() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "block";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+    
+    
+}
+document.getElementById("btn6").addEventListener("click",showtable6);
+function showtable6() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "block";
+    document.getElementById("Dependants").style.display = "none";
+    
+    
+}
+document.getElementById("btn7").addEventListener("click",showtable7);
+function showtable7() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "block";
+    
+}
+document.getElementById("closebtn").addEventListener("click",closetable);
+function closetable() {
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("p_course").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("management").style.display = "none";
+    document.getElementById("Dependants").style.display = "none";
+}
+
+
